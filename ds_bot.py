c4134ae3c364791b7520d0ac6d1c7101b75ce24f
xmodling <70622770+xmodling@users.noreply.github.com>
2020-11-13 23:07:31 +0300

    Update ds_bot.py

--- ds_bot.py
+++ ds_bot.py
@@ -3,22 +3,18 @@ import discord
 from discord.ext import commands
 import random
 import os
+
 file = open("quotes.txt", 'r')
 a = file.readlines()
-async def on_ready():
-	print('Авторизован!')
-async def on_message(self,message):
-        print(message)
-        print('Получено сообщение')
-
 client = commands.Bot(command_prefix = '/')
 @client.command(pass_context = True)
 async def gen(ctx):
-        await ctx.send(random.choice(a))
-        print(ctx.send)
+        author = ctx.message.author
+        await ctx.send(f'{author.mention},' + " " + "волкодав сгенерировал для тебя убийственную цитату:" + ":point_up:" + random.choice(a))
 @client.command(pass_context = True)
 async def code(ctx):
-        await ctx.send("Не дам код")
+        author = ctx.message.author
+        await ctx.send(f'{author.mention},' + " " + "Не дам код")
 
 token = os.environ.get('BOT_TOKEN')
 client.run(token)
